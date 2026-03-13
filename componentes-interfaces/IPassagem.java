0bd2bdc051a1baa42f0f833b713e6f7a82e6c57e
Letizia L. Baptistella <145225061+le-bap@users.noreply.github.com>
2026-03-13 23:52:43 +0000

    arrumando classe das passagens

--- componentes-interfaces/IPassagem.java
+++ componentes-interfaces/IPassagem.java
@@ -1,7 +1,15 @@
+/*
+PassagemService
+- ListarPassagensAereas()
+- ListarPassagensMaritimas()
+- FiltrarPassagens(filtros)
+- ObterPassagemPorId(idPassagem)
+*/
+
 import java.util.List;
 import java.util.Map;
 
-public interface IPassagemService {
+public interface IPassagem {
     List<PassagemAerea> listarPassagensAereas();
     List<Passagem> filtrarPassagens(Map<String, String> filtros);
     Passagem obterPassagemPorId(int idPassagem);
--- componentes-interfaces/PassagemAerea.java
+++ componentes-interfaces/PassagemAerea.java
@@ -1,10 +1,6 @@
-public class PassagemAerea extends Passagem {
-    public String classeVoo;
-    public String aeroportoEmbarque;
+import java.util.ArrayList;
+import java.util.List;
 
-    public PassagemAerea(int id, String origem, String destino, double preco, String classeVoo, String aeroportoEmbarque) {
-        super(id, origem, destino, preco);
-        this.classeVoo = classeVoo;
-        this.aeroportoEmbarque = aeroportoEmbarque;
-    }
+public class PassagemAerea implements IPassagem {
+
 }
